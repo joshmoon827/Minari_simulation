9ad531bfd4b5e672c56babcfae871d71b3359fc5
moon jooho <113758772+joshmoon827@users.noreply.github.com>
2024-09-30 10:08:48 +0900

    Update simScript.js

--- simulator/simScript.js
+++ simulator/simScript.js
@@ -1,71 +1,65 @@
 const quizData = [
-  {
-    age_group: "20대",
-    question: "친구가 돈을 값지 않는다, 어떻게 돌려받을까?",
-    options: [
-      { text: "친구? 난 믿지 않으므로 납부일을 정한다.", score: 1 },
-      { text: "최대한 늦게 돌려받아 올라갈 환율을 노린다.", score: 2 },
-      { text: "친구의 돈을 받기 위해 뒷통수를 노린다.", score: 3 },
-    ],
-    economic_terms: ["납부일", "환율"],
-  },
-  {
-    age_group: "20대",
-    question: "내 첫 아르바이트를 했는데 손해를 본것 같다</br>뭐 때문이지..?",
-    options: [
-      { text: "시간당 임금을", score: 1 },
-      { text: "아르바이트 기간 검토 했다.", score: 2 },
-      { text: "4대 보험이 보장되어있는지 확인했다.", score: 3 },
-      { text: "일하게 될 곳의 분위기가 어떤지 살폈다.", score: 4 },
-    ],
-    economic_terms: ["임금", "보험"],
-  },
-  {
-    age_group: "30대",
-    question:
-      "이젠 결혼을 앞두고 있는데… 집을 사기위한 혜택중 무엇을 골라야 할까?",
-    options: [
-      { text: "청년 안심 주택", score: 1 },
-      { text: "혼인 양도세 공제", score: 2 },
-      { text: "디딤돌 대출", score: 3 },
-    ],
-    economic_terms: ["주택", "양도세", "공제", "대출"],
-  },
-  {
-    age_group: "40대",
-    question:
-      "난 내가 결혼할지도 몰랐는데… 아이가 생길 것 같다..! 어떤게 가장 많은 혜택을 받을 수 있을까?",
-    options: [
-      { text: "첫 만남 이용권", score: 1 },
-      { text: "부모급여", score: 2 },
-      { text: "아동수당", score: 3 },
-    ],
-    economic_terms: ["일시불"],
-  },
-  {
-    age_group: "50대",
-    question:
-      "회사에서 정리해고 당해버렸다… 돈도 어느정도 모았으니 창업을 해봐야겠다. 어떤걸 알아보는게 좋을까?",
-    options: [
-      { text: "창업지원금", score: 1 },
-      { text: "폴리텍 신중년 특화 과정", score: 2 },
-      { text: "창업 도약 패키지", score: 3 },
-    ],
-  },
-  {
-    age_group: "50대",
-    question: "노후 준비 해야할 나 지금껏 안했는데 어떡하지???",
-    options: [
-      { text: "국민 연금 드가자!!!", score: 1 },
-      {
-        text: "새로운 일들을 해보고 싶어… 다른 일자리를 찾으러 떠나기",
-        score: 2,
-      },
-      { text: "귀촌을 해 장려금 1200만원을 받는다.", score: 3 },
-    ],
-    economic_terms: ["연금", "장려금"],
-  },
-];
+    {
+      age_group: "20대",
+      question: "친구가 돈을 값지 않는다, 어떻게 돌려받을까?",
+      options: [
+        { text: "친구? 난 믿지 않으므로 납부일을 정한다.", score: 2 },
+        { text: "최대한 늦게 돌려받아 올라갈 환율을 노린다.", score: 3 },
+        { text: "친구의 돈을 받기 위해 뒷통수를 노린다.", score: 1 },
+      ],
+      economic_terms: ["납부일", "환율"],
+    },
+    {
+      age_group: "20대",
+      question: "내 첫 아르바이트를 했는데 손해를 본것 같다</br>뭐 때문이지..?",
+      options: [
+        { text: "시간당 임금을 9500원 수령 받았다", score: 4 },
+        { text: "하루에 7시간을 일했다", score: 3 },
+        { text: "4대 보험이 보장되어있는지 확인했다.", score: 1 },
+        { text: "돈을 계좌 이체로 받았다", score: 2 },
+      ],
+      economic_terms: ["임금", "보험"],
+    },
+    {
+      age_group: "30대",
+      question: "이젠 결혼을 앞두고 있는데… 집을 사기위한 혜택중 무엇을 골라야 할까?",
+      options: [
+        { text: "청년 안심 주택", score: 2 },
+        { text: "혼인 양도세 공제", score: 3 },
+        { text: "디딤돌 대출", score: 1 },
+      ],
+      economic_terms: ["주택", "양도세", "공제", "대출"],
+    },
+    {
+      age_group: "40대",
+      question: "난 내가 결혼할지도 몰랐는데… 아이 소식이 들어왔다. 어떤 혜택을 받아야 할까?",
+      options: [
+        { text: "첫 만남 이용권", score: 2 },
+        { text: "부모급여", score: 3 },
+        { text: "아동수당", score: 1 },
+      ],
+      economic_terms: ["아동 수당"],
+    },
+    {
+      age_group: "50대",
+      question: "회사에서 정리해고 당해버렸다… 돈도 어느정도 모았으니 창업을 해봐야겠다. 어떤걸 알아보는게 좋을까?",
+      options: [
+        { text: "창업지원금", score: 1 },
+        { text: "폴리텍 신중년 특화 과정", score: 2 },
+        { text: "창업 도약 패키지", score: 3 },
+      ],
+    },
+    {
+      age_group: "50대",
+      question: "노후 준비 해야할때인데 뭐부터 해야할까?",
+      options: [
+        { text: "국민 연금으로 대비한다", score: 2 },
+        { text: "개인 연금을 가입한다", score: 3 },
+        { text: "『안한다』", score: 1 },
+      ],
+      economic_terms: ["연금"],
+    },
+  ];
 
 const questionContainer = document.getElementById("question-container");
 const optionsContainer = document.getElementById("options-container");
