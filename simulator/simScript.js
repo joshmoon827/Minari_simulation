94ae4bdf8144335ed4d0fcddf2306fc21337e84a
joshmoon827 <joshmoon827@gmail.com>
2024-10-21 02:38:26 +0900

    지ㄴ짜 최종 마무리
    제발...
    (어짜피 나중에 url 숨기려고 커밋 더할 것 같긴 함)

--- simulator/simScript.js
+++ simulator/simScript.js
@@ -1,7 +1,7 @@
 const quizData = [
     {
       age_group: "20대",
-      question: "다음중 최고의 학교는? ( 점수 안들어감 )",
+      question: "1/9<br/><br/>다음중 최고의 학교는? ( 점수 안들어감 )",
       options: [
         { text: "**대구소프트웨어마이스터고등학교**", score: 1 },
         { text: "광주소프트웨어마이스터고등학교", score: 1 },
@@ -11,7 +11,7 @@ const quizData = [
     },
     {
       age_group: "20대",
-      question: "가입 시점 외에는 돈을 넣을 수 없는 예금은?",
+      question: "2/9<br/><br/>가입 시점 외에는 돈을 넣을 수 없는 예금은?",
       options: [
         { text: "정기 예금", score: 4 },
         { text: "보통 예금", score: 3 },
@@ -22,7 +22,7 @@ const quizData = [
     },
     {
       age_group: "30대",
-      question: "이젠 결혼을 앞두고 있는데… 집을 사기위한 혜택중 무엇을 골라야 할까?",
+      question: "3/9<br/><br/>이젠 결혼을 앞두고 있는데… 집을 사기위한 혜택중 무엇을 골라야 할까?",
       options: [
         { text: "청년 안심 자택", score: 2 },
         { text: "혼인 양도세 공제", score: 3 },
@@ -32,7 +32,7 @@ const quizData = [
     },
     {
       age_group: "40대",
-      question: "‘1달러=1000원’에서 ‘1달러=2000원’이 됐다고 가정했을 때, 잘못된 해석은?",
+      question: "4/9<br/><br/>‘1달러=1000원’에서 ‘1달러=2000원’이 됐다고 가정했을 때, 잘못된 해석은?",
       options: [
         { text: "원화값 상승", score: 4 },
         { text: "원달러 환율 상승", score: 1 },
@@ -43,7 +43,7 @@ const quizData = [
     },
     {
       age_group: "50대",
-      question: "현재 한국은행 기준금리는 얼마일까?",
+      question: "5/9<br/><br/>현재 한국은행 기준금리는 얼마일까?",
       options: [
         { text: "연 3.0%", score: 1 },
         { text: "연 3.5%", score: 3 },
@@ -52,7 +52,7 @@ const quizData = [
     },
     {
       age_group: "50대",
-      question: "점포를 거래할 때 기존 점주가 쌓은 영업 노하우와 단골 등을 이어받는 명목으로 지급하는 돈은?",
+      question: "6/9<br/><br/>점포를 거래할 때 기존 점주가 쌓은 영업 노하우와 단골 등을 이어받는 명목으로 지급하는 돈은?",
       options: [
         { text: "권리금", score: 1 },
         { text: "중도금", score: 3 },
@@ -62,7 +62,7 @@ const quizData = [
     },
     {
       age_group: "50대",
-      question: "강력한 경쟁자가 등장했을 때 기존 기업들의 경쟁력이 오히려 강해지는 현상을 가리키는 용어는?",
+      question: "7/9<br/><br/>강력한 경쟁자가 등장했을 때 기존 기업들의 경쟁력이 오히려 강해지는 현상을 가리키는 용어는?",
       options: [
         { text: "풍선효과", score: 1 },
         { text: "기저효과", score: 1 },
@@ -72,7 +72,7 @@ const quizData = [
     },
     {
       age_group: "50대",
-      question: "어떤 산업이 장기 호황을 맞거나 어떤 자산 가격이 대세 상승장을 맞았을 때 쓰는 표현은?",
+      question: "8/9<br/><br/>어떤 산업이 장기 호황을 맞거나 어떤 자산 가격이 대세 상승장을 맞았을 때 쓰는 표현은?",
       options: [
         { text: "퍼펙트 스톰", score: 1 },
         { text: "캐리트레이드", score: 1 },
@@ -83,7 +83,7 @@ const quizData = [
     },
     {
       age_group: "50대",
-      question: "청소년만 선택 :: 경제를 배우고 싶은 의향이 있나요? <br/> ( 점수 안들어감 )",
+      question: "⚠️ 청소년만 선택(마지막) ⚠️ <br/><br/><br/>  경제를 배우고 싶은 의향이 있나요?  <br/> ( 점수 안들어감 )<br/><br/>",
       options: [
         { text: "있다", score: 0, result: 1 },
         { text: "없다", score: 0,result: 0 },
